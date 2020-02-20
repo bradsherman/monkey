9ada13fa2e2edfaa3584c69cf9126cdb2c258607
Brad Sherman <bsherman1096@gmail.com>
2020-02-20 05:46:01 +0000

    Began evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"github.com/bradsherman/monkey/lexer"
+	"github.com/bradsherman/monkey/evaluator"
 	"github.com/bradsherman/monkey/parser"
 )
 
@@ -40,8 +41,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
